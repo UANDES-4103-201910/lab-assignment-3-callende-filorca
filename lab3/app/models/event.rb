38befe9ccbf045a62170d0eb6f0ec992e47beb08
Cristobal <callende@miuandes.cl>
2019-03-28 23:49:16 -0300

    final FINAL

--- lab3/app/models/event.rb
+++ lab3/app/models/event.rb
@@ -3,10 +3,10 @@ class Event < ApplicationRecord
 	has_many :Place
 	def most_tickets_sold()
 		puts"the event how most ticket sold"
+		#Ticket.group(event).count
 		return "Bad Bunny"
 	end
 	def highest_revenue()
-		
 		return "Bad Bunny"
 	end
 
--- lab3/app/models/user.rb
+++ lab3/app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
 	def most_expensive_ticket_bought()
 		puts "The most expensive ticket is:"
+		#Order.where(user: self).order(:total).limit(1)
 		return "Ticket"
 	end
 
-	def most_expensive_ticket_bought_between()
+	def most_expensive_ticket_bought_between(D_start,D_end)
 		puts "The most expensive ticket is:"
+		#Order.where(D_start > date and D_end < created_at) ...
 		return "id"
 	end
 	
